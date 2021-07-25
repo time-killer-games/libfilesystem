952de4d67d566e029b628314e55b29ab94a9f064
time-killer-games <samuelvenable@hotmail.com>
2021-07-25 00:34:35 -0400

    Update FileSystem (MacOSX x64).sh

--- FileSystem (MacOSX x64).sh
+++ FileSystem (MacOSX x64).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "FileSystem (x64)"
 mkdir "FileSystem (x64)/MacOSX"
-clang++ "FileSystem.cpp" "FileSystem/MacOSX/filesystem.cpp" "FileSystem/POSIX/filesystem.cpp" "FileSystem/Universal/filesystem.cpp" -o "FileSystem (x64)/MacOSX/libfilesystem.dylib" -std=c++17 -shared -fPIC -m64
+clang++ "FileSystem.cpp" "FileSystem/MacOSX/filesystem.cpp" "FileSystem/POSIX/filesystem.cpp" "FileSystem/Universal/filesystem.cpp" -o "FileSystem (x64)/MacOSX/libfilesystem.dylib" -std=c++17 -shared -fPIC -arch arm64 -arch x86_64

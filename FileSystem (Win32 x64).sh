371892113db018a3e74c36312ad7e6d747ef8fce
Samuel Venable <samuelvenable@hotmail.com>
2021-10-19 23:30:41 -0400

    Delete FileSystem (Win32 x64).sh

--- FileSystem (Win32 x64).sh
+++
@@ -1,5 +0,0 @@
-cd "${0%/*}"
-
-mkdir "FileSystem (x64)"
-mkdir "FileSystem (x64)/Win32"
-g++ "FileSystem.cpp" "FileSystem/Win32/filesystem.cpp" "FileSystem/Universal/filesystem.cpp" -o "FileSystem (x64)/Win32/libfilesystem.dll" -std=c++17 -shared -static-libgcc -static-libstdc++ -static -fPIC -m64

41a697a7aea8bc98611680e550f486ad653f00ec
Samuel Venable <samuelvenable@hotmail.com>
2021-10-19 23:30:17 -0400

    Delete FileSystem (FreeBSD x86).sh

--- FileSystem (FreeBSD x86).sh
+++
@@ -1,5 +0,0 @@
-cd "${0%/*}"
-
-mkdir "FileSystem (x86)"
-mkdir "FileSystem (x86)/FreeBSD"
-g++ "FileSystem.cpp" "FileSystem/FreeBSD/filesystem.cpp" "FileSystem/POSIX/filesystem.cpp" "FileSystem/Universal/filesystem.cpp" -o "FileSystem (x86)/FreeBSD/libfilesystem.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -fPIC -m32

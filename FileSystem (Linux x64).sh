52706c4f6813ec408e2223139a454a3e789cce5f
Samuel Venable <samuelvenable@hotmail.com>
2021-10-19 23:30:29 -0400

    Delete FileSystem (Linux x64).sh

--- FileSystem (Linux x64).sh
+++
@@ -1,5 +0,0 @@
-cd "${0%/*}"
-
-mkdir "FileSystem (x64)"
-mkdir "FileSystem (x64)/Linux"
-g++ "FileSystem.cpp" "FileSystem/Linux/filesystem.cpp" "FileSystem/POSIX/filesystem.cpp" "FileSystem/Universal/filesystem.cpp" -o "FileSystem (x64)/Linux/libfilesystem.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -fPIC -m64

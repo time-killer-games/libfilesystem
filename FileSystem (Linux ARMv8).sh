44e4b714d6949b192d7534a1300bb7fc35af0271
Samuel Venable <samuelvenable@hotmail.com>
2021-10-19 23:30:25 -0400

    Delete FileSystem (Linux ARMv8).sh

--- FileSystem (Linux ARMv8).sh
+++
@@ -1,5 +0,0 @@
-cd "${0%/*}"
-
-mkdir "FileSystem (ARMv8)"
-mkdir "FileSystem (ARMv8)/Linux"
-g++ "FileSystem.cpp" "FileSystem/Linux/filesystem.cpp" "FileSystem/POSIX/filesystem.cpp" "FileSystem/Universal/filesystem.cpp" -o "FileSystem (ARMv8)/Linux/libfilesystem.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -fPIC

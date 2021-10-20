cf685572f75147c0b4b0d416c0ace277bb28d305
Samuel Venable <samuelvenable@hotmail.com>
2021-10-20 01:21:57 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ cd "${0%/*}";
 if [ $(uname) = "Darwin" ]; then
   clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.dylib" -shared -std=c++17 -arch arm64 -arch x86_64; 
 elif [ $(uname) = "Linux" ]; then
-  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -static-libgcc -static-libstdc++;
+  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -static-libgcc -static-libstdc++ -fPIC;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -lc;
+  clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -lc -fPIC;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -static-libgcc -static-libstdc++ -lc;
+  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -static-libgcc -static-libstdc++ -lc -fPIC;
 else
   g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.dll" -shared -std=c++17 -static-libgcc -static-libstdc++ -static;
 fi;

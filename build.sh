fac2e6ac4f9ad053f89c17e71b5777b4076af108
Samuel Venable <samuelvenable@hotmail.com>
2021-12-14 03:55:33 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ elif [ $(uname) = "Linux" ]; then
 elif [ $(uname) = "FreeBSD" ]; then
   clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -lc -fPIC;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -static-libgcc -static-libstdc++ -lc -fPIC;
+  clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++17 -lc -fPIC;
 else
   g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.dll" -shared -std=c++17 -static-libgcc -static-libstdc++ -static;
 fi;

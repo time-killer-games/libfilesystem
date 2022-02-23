a456e3022e7680a3220327dbef3f564f4142429d
Samuel Venable <samuelvenable@hotmail.com>
2022-02-22 17:48:34 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ cd "${0%/*}";
 if [ $(uname) = "Darwin" ]; then
   clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.dylib" -shared -std=c++20 -arch arm64 -arch x86_64; 
 elif [ $(uname) = "Linux" ]; then
-  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++20 -static-libgcc -static-libstdc++ -fPIC;
+  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++20 -static-libgcc -static-libstdc++ -lpthread -fPIC;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++20 -lc -fPIC;
+  clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++20 -lc -lpthread -fPIC;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++20 -static-libgcc -static-libstdc++ -fPIC;
+  g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++20 -static-libgcc -static-libstdc++ -lpthread -fPIC;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++20 -lc -fPIC;
+  clang++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.so" -shared -std=c++20 -lc -lpthread -fPIC;
 else
   g++ "filesystem.cpp" "gamemaker.cpp" -o "libfilesystem.dll" -shared -std=c++20 -static-libgcc -static-libstdc++ -static;
 fi;

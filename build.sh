01d0a7f343a4638db2247d26a358815fb9d7d556
Samuel Venable <samuelvenable@hotmail.com>
2022-05-07 07:03:09 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ elif [ $(uname) = "FreeBSD" ]; then
 elif [ $(uname) = "DragonFly" ]; then
   g++ -c "filesystem.cpp" "gamemaker.cpp" -shared -std=c++17 -static-libgcc -static-libstdc++ -lpthread -fPIC && g++ "filesystem.o" "gamemaker.o"  -o "libfilesystem.so" -shared -std=c++17 -static-libgcc -static-libstdc++ -lpthread -fPIC;
   ar rc libfilesystem.a gamemaker.o filesystem.o && rm -f "gamemaker.o" "filesystem.o";
+elif [ $(uname) = "NetBSD" ]; then
+  g++ -c "filesystem.cpp" "gamemaker.cpp" -shared -std=c++17 -static-libgcc -static-libstdc++ -lpthread -fPIC && g++ "filesystem.o" "gamemaker.o"  -o "libfilesystem.so" -shared -std=c++17 -static-libgcc -static-libstdc++ -lpthread -fPIC;
+  ar rc libfilesystem.a gamemaker.o filesystem.o && rm -f "gamemaker.o" "filesystem.o";
 elif [ $(uname) = "OpenBSD" ]; then
   clang++ -c "filesystem.cpp" "gamemaker.cpp" -shared -std=c++17 -lc -lpthread -fPIC && clang++ "filesystem.o" "gamemaker.o"  -o "libfilesystem.so" -shared -std=c++17 -lc -lpthread -fPIC;
   ar rc libfilesystem.a gamemaker.o filesystem.o && rm -f "gamemaker.o" "filesystem.o";

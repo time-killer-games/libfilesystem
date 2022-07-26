d1ce8c5ebfb7b53f7c7347ccb00fa502f7629314
Samuel Venable <samuelvenable@hotmail.com>
2022-07-26 15:34:54 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ elif [ $(uname) = "NetBSD" ]; then
   g++ -c "filesystem.cpp" "gamemaker.cpp" -shared -std=c++17 -static-libgcc -static-libstdc++ -lpthread -fPIC && g++ "filesystem.o" "gamemaker.o"  -o "libfilesystem.so" -shared -std=c++17 -static-libgcc -static-libstdc++ -lpthread -fPIC;
   ar rc libfilesystem.a gamemaker.o filesystem.o && rm -f "gamemaker.o" "filesystem.o";
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ -c "filesystem.cpp" "gamemaker.cpp" -shared -std=c++17 -lc -lkvm -lpthread -fPIC && clang++ "filesystem.o" "gamemaker.o"  -o "libfilesystem.so" -shared -std=c++17 -lc -lpthread -fPIC;
+  clang++ -c "filesystem.cpp" "gamemaker.cpp" -shared -std=c++17 -lc -lkvm -lpthread -fPIC && clang++ "filesystem.o" "gamemaker.o"  -o "libfilesystem.so" -shared -std=c++17 -lc -lkvm -lpthread -fPIC;
   ar rc libfilesystem.a gamemaker.o filesystem.o && rm -f "gamemaker.o" "filesystem.o";
 else
   g++ -c "filesystem.cpp" "gamemaker.cpp" -shared -std=c++17 -static-libgcc -static-libstdc++ -lshell32 -lole32 -luuid -static && g++ "filesystem.o" "gamemaker.o"  -o "libfilesystem.dll" -shared -std=c++17 -static-libgcc -static-libstdc++ -lshell32 -lole32 -luuid -static;
